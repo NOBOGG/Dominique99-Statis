9bf5408736e266763cc4a39888c57e7e3374903b
NOBOGG <oindrawan@student.ciputra.ac.id>
2022-10-20 11:18:06 +0700

    first commit

---
+++ storage/framework/views/5c465cb0483317f41d833ff51fa6bba5150d0527.php
@@ -0,0 +1,10 @@
+
+<?php $__env->startSection('title','Home'); ?>
+
+<?php $__env->startSection('content'); ?>
+<div class="container mt-2">
+    <h2>Home Page</h2>
+</div>
+    
+<?php $__env->stopSection(); ?>
+<?php echo $__env->make('layouts/mainlayout', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH D:\Xampp\htdocs\Laravel Project\webstatisDominique\resources\views/index.blade.php ENDPATH**/ ?>
