d24f6ce89bd2ddb2a49afb59d50ef088adb1c1fb
paiN2104 <sreynaldo@student.ciputra.ac.id>
2022-10-24 22:18:39 +0700

    fix sizing font by adding new class to css

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,7 +5,7 @@
 <div class="container mt-2">
     <img class="w-100 img-fluid" src="images/BannerDominique.png" alt="">
 
-    <h1 class="text-center mt-2">Favorite Menu</h1>
+    <h1 class="text-center mt-2 sizing">Favorite Menu</h1>
 </div>
     
 @endsection
