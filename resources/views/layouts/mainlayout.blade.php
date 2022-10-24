5c981b27b65ec71c652fbac4c5334b8d86fe4d41
paiN2104 <sreynaldo@student.ciputra.ac.id>
2022-10-24 21:28:49 +0700

    fix color and add font on navbar

--- resources/views/layouts/mainlayout.blade.php
+++ resources/views/layouts/mainlayout.blade.php
@@ -14,8 +14,12 @@
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
+        <div class="collapse navbar-collapse " id="navbarNav">
           <ul class="navbar-nav">
+            <a class="navbar-brand logoStyling" href="#">
+              <img src="images/Dom_Logo.png" alt="Logo" width="30" height="24" class="d-inline-block align-text-top ">
+              Dominique99
+            </a>
             <li class="nav-item">
               <a class="nav-link" aria-current="page" href="/">Home</a>
             </li>
