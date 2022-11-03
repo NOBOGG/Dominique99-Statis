2f83675f1a82d857ddfd5b2c3f43e31fa255b09d
NOBOGG <oindrawan@student.ciputra.ac.id>
2022-11-03 12:36:41 +0700

    Finishing

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -15,16 +15,16 @@
                     <h5>Alamat Toko</h5>
                 </div>
                 <div class="row">
-                    <h6>Jalan K.H.mansyur 55, Probolinggo</h6>
+                    <h6>Jalan K.H.Mansyur 55, Probolinggo</h6>
                 </div>
             </div>
             <div class="col-lg-2 col-md-2 col-sm-12">
                 <h5 class="me-5">Reach Us On</h5>
                 <div class="row afooter">
                     <div class="col">
-                        <a href="https://it-it.facebook.com/dominique99.id/"><i class="bi bi-facebook"></i></a>
-                        <a href="https://wa.me/082229442338?text=Hello%20Dominique99"><i class="bi bi-whatsapp ms-1"></i></a>
-                        <a href="https://www.instagram.com/dominique99.id"><i class="bi bi-instagram ms-1"></i></a>
+                        <a href="https://it-it.facebook.com/dominique99.id/"><i class="bi bi-facebook text-white"></i></a>
+                        <a href="https://wa.me/082229442338?text=Hello%20Dominique99"><i class="bi bi-whatsapp ms-1 text-white"></i></a>
+                        <a href="https://www.instagram.com/dominique99.id"><i class="bi bi-instagram ms-1 text-white"></i></a>
                     </div>
                 </div>
             </div>
